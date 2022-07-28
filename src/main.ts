ffe66ccb735d30fb554ca29a2cc1f01c983ae925
Octavius-Palmqvist <octaviuscodes@gmail.com>
2022-07-28 14:01:34 +0200

    firebase integration and deployment

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,28 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/global.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDNJoP3dThVUnLq3flCmIoXmjq489jH-Ig",
+  authDomain: "portfolio-a121a.firebaseapp.com",
+  projectId: "portfolio-a121a",
+  storageBucket: "portfolio-a121a.appspot.com",
+  messagingSenderId: "711540470512",
+  appId: "1:711540470512:web:1f8f1cf7a4e5a5e4868835",
+  measurementId: "G-1KF7WMX9J6"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+
 const app = createApp(App)
 
 app.component('NavigationBar', NavigationBar)
